0ea4d514ce0acd97c7b0dc36c88779c733a2ed7d
harrypotter0045 <harrypotter0045@gmail.com>
2020-11-20 04:23:54 -0600

    Admin can create and update product + category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_20_095009) do
+ActiveRecord::Schema.define(version: 2020_11_20_102037) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2020_11_20_095009) do
     t.decimal "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
   end
 
 end
