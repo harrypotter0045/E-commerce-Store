8f9cc88d113ee5e91ff6b6fdfc229d58843a767c
harrypotter0045 <harrypotter0045@gmail.com>
2020-11-27 01:13:36 -0600

    Fix validation errors and add csv files

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :category
+  validates :name, presence: true
+  validates :model_year, numericality: { greater_than: 2010, only_integer: true }
+  validates :price, numericality: {greater_than_or_equal_to: 0}
   has_one_attached :product_image
 end
