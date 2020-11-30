7a5d3681d5c6ea59d963930ed11899c17bf6767d
harrypotter0045 <harrypotter0045@gmail.com>
2020-11-30 11:58:05 -0600

    Implement search based on name and category

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,12 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def search
+    if params[:category] == "None"
+      @products = Product.where("name LIKE ?", "%#{params[:search_term]}%")
+    else
+      @products = Product.where("name LIKE ? and category_id = #{params[:category]}", "%#{params[:search_term]}%")
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :products, only: :show
   resources :categories, only: :show
+
+  get "search", to: "products#search", as: "search" # search_path
+
   root to: "categories#index"
 
   devise_for :admin_users, ActiveAdmin::Devise.config
